584406adddac70a0a637d77f2d803935a0f301ac
rizry <rizzwan.ansarii@gmail.com>
2022-10-02 14:49:02 +0530

    added linkedhashmap operations

--- Main.java
+++ Main.java
@@ -13,9 +13,22 @@ public class Main {
       else value += 1;
       hashMap.add(word, value);
     }
-
     System.out.println(hashMap);
     System.out.println("frequency of 'to': " + hashMap.get("to"));
+
+    
+    str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+    MyLinkedHashMap<String, Integer> linkedHashMap = new MyLinkedHashMap<>();
+    strArr = str.toLowerCase().split(" ");
+    for (String word : strArr) {
+      Integer value = linkedHashMap.get(word);
+      if (value == null) value = 1;
+      else value += 1;
+      linkedHashMap.add(word, value);
+    }
+    System.out.println(linkedHashMap);
+    System.out.println("frequency of 'paranoid': " + linkedHashMap.get("paranoid"));
+
   }
 
 }
